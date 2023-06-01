311fa8545bf4041ca3c0a224d60ca9ade0253f3d
Abhishek <jadonabhi0@gmail.com>
2023-06-01 22:03:00 +0530

    updated commit

--- src/Ruff.java
+++ src/Ruff.java
@@ -1,48 +1,28 @@
 import java.util.*;
 
-public class Ruff {
-
-   public static int encode(String str){
-       StringBuilder sb = new StringBuilder();
-       int count = 1;
-       for(int i = 1 ; i < str.length() ; i++){
-           if (str.charAt(i) == str.charAt(i-1)){
-               count++;
-           }else{
-               if (count == 1){
-                   sb.append(str.charAt(i-1));
-               }else{
-                   sb.append(str.charAt(i-1)+""+count);
-                   count = 1;
-               }
-           }
-       }
-       if (str.charAt(str.length()-1) != str.charAt(str.length()-2)){
-           sb.append(str.charAt(str.length()-1));
-       }else {
-           sb.append(str.charAt(str.length()-1)+""+count);
-       }
-//       System.out.println(sb);
-       return sb.length();
-   }
-
-
-   public static int helper(String str, int k, int l){
-       if (str.length() == 0) return 0;
-       if (k == 0) return encode(str);
-       int c = 0;
-       for(int i = 0 ; i < l ; i++){
-           String s = str.substring(0,i)+str.substring(i+1);
-           int a = helper(s, k-1, l);
-           int b = helper(str, k, l);
-           c = Math.min(a, b);
-       }
-       return c;
-   }
+public class Ruff{
+   static int cc=0;
+    public static void generateParanthesis(int n, String str, int o, int c){
+        if(n*2 == str.length()) {
+            ++cc;
+            System.out.println(str+" "+ cc);
+            return;
+        }
+        if(o<n)generateParanthesis(n, str+"{", o+1, c);
+        if (c < o)generateParanthesis(n, str+"}", o, c+1);
+    }
 
     public static void main(String[] args) {
 
+        generateParanthesis(5,"", 0, 0);
 
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        });
 
     }
 }
